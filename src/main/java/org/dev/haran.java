d6fd527f63709402787c6776fc3a1ca74ba83c19
mjhharan <mjhharan@yahoo.com>
2025-04-23 07:14:23 +0530

    haran_2code

--- src/main/java/org/dev/haran.java
+++ src/main/java/org/dev/haran.java
@@ -3,6 +3,9 @@ package org.dev;
 public class haran {
 public static void main(String[] args) {
 	System.out.println("kvsdjj");
+	System.out.println("kvsd55jj");
+	System.out.println("kv22sdjj");
+	System.out.println("kvsd44jj");
 }
 
 }
