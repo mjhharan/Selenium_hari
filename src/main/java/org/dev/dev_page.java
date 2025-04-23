52b695938468f65d6ec1c61c5cfdc1199a3256ee
mjhharan <mjhharan@yahoo.com>
2025-04-23 07:30:04 +0530

    code1

--- src/main/java/org/dev/dev_page.java
+++ src/main/java/org/dev/dev_page.java
@@ -3,5 +3,8 @@ package org.dev;
 public class dev_page {
 public static void main(String[] args) {
 	System.out.println("hari");
+	System.out.println("fsd22gs");
+	System.out.println("fsdg33s");
+	System.out.println("fsd44gs");
 }
 }
--- src/main/java/org/dev/haran.java
+++ src/main/java/org/dev/haran.java
@@ -9,4 +9,4 @@ public static void main(String[] args) {
 }
 
 }
-}
+
